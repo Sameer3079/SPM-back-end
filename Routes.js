d6d8316000df841d74d2b01d24669853a84238d1
Sameer Mohamed <Sameer3079>
2018-08-29 20:34:37 +0530

    Partially implemented the schedule viva function

--- Routes.js
+++ Routes.js
@@ -1,8 +1,10 @@
 const Express       = require("express");
 const Routes        = Express.Router();
 const StudentRoute  = require('./Student/Student.Route');
+const AdminRoute = require('./Admin/Admin.Route')
 
 
 Routes.use('/student', StudentRoute);
+Routes.use('/admin', AdminRoute);
 
 module.exports = Routes;
