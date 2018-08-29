4dae39acf0ad63e11055d79aa3dc3b1896b690fa
Sameer3079 <smrbasil4@gmail.com>
2018-08-29 19:45:49 +0530

    Reverted accidental commit

--- Routes.js
+++ Routes.js
@@ -1,10 +1,8 @@
 const Express       = require("express");
 const Routes        = Express.Router();
 const StudentRoute  = require('./Student/Student.Route');
-const AdminRoute = require('./Admin/Admin.Route')
 
 
 Routes.use('/student', StudentRoute);
-Routes.use('/admin', AdminRoute);
 
 module.exports = Routes;
