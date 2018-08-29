d9d9e8d53f6c59e82c26b96d2bfc6abdc26cfcb4
Sameer3079 <smrbasil4@gmail.com>
2018-08-29 20:59:00 +0530

    Revert "Partially implemented the schedule viva function"
    This reverts commit d6d8316000df841d74d2b01d24669853a84238d1.

--- Config/DBSchema.js
+++ Config/DBSchema.js
@@ -2,25 +2,13 @@ const Mongoose      = require("mongoose");
 const Schema = Mongoose.Schema;
 
 const StudentSchema = new Schema({
-    studentId: {
-        type: String,
-        required: true
-    },
     name:{
         type:String,
         required:true
     }
 });
 
-// Sameer
-let StudentVivaSchedule = new Schema({
-    studentId: { type: String, required: true },
-    dateTime: { type: Date, required: true },
-    location: { type: String, required: true }
-})
-
 Mongoose.model('Student', StudentSchema);
-Mongoose.model('StudentVivaSchedule', StudentVivaSchedule)
 
 Mongoose.connect('mongodb://localhost:27017/Student', { useNewUrlParser: true }, (err) => {
     if(err){
