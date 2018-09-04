bcb476885c21b95668f56865456d82863f2f3295
sachithkk <tharakakk@gmail.com>
2018-09-05 00:05:36 +0530

    Create company schema

--- Config/DBSchema.js
+++ Config/DBSchema.js
@@ -1,16 +1,59 @@
 const Mongoose      = require("mongoose");
 const Schema = Mongoose.Schema;
 
+/**
+ *  Student Schema.
+ */
 const StudentSchema = new Schema({
-    studentId: {
-        type: String,
-        required: true
+    studentId:{
+        type:String,
+        required:true
     },
-    name:{
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    company:{
+        type:String,
+        required:true
+    },
+    supervisor:{
+        type:String,
+        required:true
+    },
+    academicYear:{
+        type:Number,
+        required:false
+    }
+});
+
+
+/**
+ *  Company Schema.
+ */
+
+const CompanySchema = new Schema({
+
+    companyName:{
+        type:String,
+        required:true
+    },
+    spervisorName:{
+        type:String,
+        required:true
+    },
+    companyContactNum:{
         type:String,
         required:true
     }
+
+
 });
+
 
 // Sameer
 let StudentVivaSchedule = new Schema({
@@ -20,7 +63,8 @@ let StudentVivaSchedule = new Schema({
 })
 
 Mongoose.model('Student', StudentSchema);
-Mongoose.model('StudentVivaSchedule', StudentVivaSchedule)
+Mongoose.model('StudentVivaSchedule', StudentVivaSchedule);
+Mongoose.model('Company', CompanySchema);
 
 Mongoose.connect('mongodb://localhost:27017/Student', { useNewUrlParser: true }, (err) => {
     if(err){
