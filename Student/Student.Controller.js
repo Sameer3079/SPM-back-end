d8f28280275f8a2db1c99beb8069ab3bc4f6b8a3
Sameer3079 <smrbasil4@gmail.com>
2018-09-04 23:14:57 +0530

    Implemented the getAll, getOne, update & delete methods for the viva schedules

--- Student/Student.Controller.js
+++ Student/Student.Controller.js
@@ -1,49 +1,48 @@
-const Mongoose      = require("../Config/DBSchema");
+const Mongoose = require("../Config/DBSchema");
 const StudentSchema = Mongoose.model("Student");
 let StudentForms = Mongoose.model('StudentForms')
-let functions = require('../functions')
 let fs = require('fs')
 let multer = require('multer')
 let formI6Directory = '../student_forms/form_i6/'
-let uploadFormI6 = multer({dest: formI6Directory}).single('word_document')
+let uploadFormI6 = multer({ dest: formI6Directory }).single('word_document')
 
-var StudentController = function(){
+var StudentController = function () {
     this.add = (Data) => {
-        return new Promise((resolve,reject) => {
+        return new Promise((resolve, reject) => {
             var Student = new StudentSchema({
                 studentId: Data.studentId,
-                name:Data.name
+                name: Data.name
             })
 
             Student.save()
-            .then(() => {
-                resolve({"status":201,"message":"Added Student"})
-            })
-            .catch((err) => {
-                reject({"status":404,"message":"Err "+err})
-            });
+                .then(() => {
+                    resolve({ "status": 201, "message": "Added Student" })
+                })
+                .catch((err) => {
+                    reject({ "status": 404, "message": "Err " + err })
+                });
         })
     }
 
     this.get = () => {
-        return new Promise((resolve,reject) => {
+        return new Promise((resolve, reject) => {
             StudentSchema.find().exec()
-            .then((Data) => {
-                resolve({"status":200,"message":"Get all data", "data":Data})
-            })
-            .catch((err) => {
-                reject({"status":404,"message":"err "+err})
-            })
+                .then((Data) => {
+                    resolve({ "status": 200, "message": "Get all data", "data": Data })
+                })
+                .catch((err) => {
+                    reject({ "status": 404, "message": "err " + err })
+                })
         })
     }
 
     // TODO: Test Method
     this.submitFormI6 = (studentId, formI6) => {
         return new Promise((resolve, reject) => {
-            functions.isStudentValid(studentId).then(validity => {
-                if (validity == true) {
+            StudentSchema.find({ studentId: studentId }).then(data => {
+                if (data.length !== 0) {
                     let document = JSON.parse(formI6)
-                    fs.writeFile('./student_forms/form_i6/'+studentId + '-form-I6', document, err => {
+                    fs.writeFile('./student_forms/form_i6/' + studentId + '-form-I6', document, err => {
                         if (err) {
                             console.log(err)
                         }
